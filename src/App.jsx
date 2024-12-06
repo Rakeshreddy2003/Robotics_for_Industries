8d92a6d3203ffccdbfc319493109558916cba3c1
Rakesh Reddy <rakeshreddysarikonda11@gmail.com>
2024-12-06 16:18:24 +0530

    fix: navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 import LandingPage from './Pages/LandingPage'
 import './App.css';
 
 function App() {
   return (
-   <Router>
+   <Router basename="/Robotics_for_Industries">
     <Routes>
-      <Route path='/' element={<LandingPage/>}/>
+      <Route path="/" element={<LandingPage />} />
     </Routes>
    </Router>
   );
